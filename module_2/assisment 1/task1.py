1b24af8fd46aad7cca1d4f00d6f59e359b0cf8ab
amanpal3 <amanpal00919@gmail.com>
2025-11-29 23:12:25 +0530

    first commit

---
+++ module_2/assisment 1/task1.py
@@ -0,0 +1,10 @@
+x=int(input("Enter fisrt number: "))
+y=int(input("Enter second number: "))
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+print("Addition:",add,/n"Subtraction:",sub,"Multiplication:",mul,"Division:",div)
+"""print("18Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)"""
---
+++ module_2/assisment 1/task_2.py
@@ -0,0 +1,5 @@
+
+x=(input("Enter first name: ") )
+y=(input("Enter second name: ") )
+add=x + y
+print("hello,",add ,"! welcome to the python programming",sep="")
